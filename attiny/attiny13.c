1ae7c858c532684874ca3f32b2d03ab9b4014555
WolfWolfWolfWolf <wolfram.liese@gmail.com>
2015-09-28 21:35:39 +0200

    Beautifying start, not tested yet

--- attiny/attiny13.c
+++ attiny/attiny13.c
@@ -15,72 +15,82 @@
 #include <avr/wdt.h>
 //#include <avr/power.h>
 
+/* initialize the watchdog */
 void wdt_init(uint8_t timeout) {
+	/* globally disable all interrupts */
 	cli();
+	/* enable the watchdog by setting the timer */
 	wdt_enable(timeout);
+	/* enable the watchdog interrupt in the watchdog timer control register. 
+	   the interrupt routine called when watchdog timer expired is below, see ISR(WDT_vect).
+	   WDE is automatically set to 0 by this operation, means that WD is in interrupt mode */
+	WDTCR |= (1<<WDTIE);   // Enable watchdog	
+	/* we could set the WDRF flag in MCUSR to bring the watchdog to reset mode (we don't want that) */
 	//MCUSR |= (1<<WDRF);
-	WDTCR |= /*(1<<WDE) | */(1<<WDTIE);   // Enable watchdog
+	/* reset the watchdog, meaning we start the timer */
 	wdt_reset();
-	//WDTCR = (1<<WDTIE) | WDTO_8S;    // Watchdog interrupt instead of reset with 4s timeout
+	/* globally enable all interrupts again */
 	sei();
 }
 
+/* put the attiny to deep power down mode */
 void sleep_now() {
-	//power_all_disable();
+	/* set the sleep mode to power down */
 	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-	sleep_enable();
-	//sleep_bod_disable();
+	/* and finally go to sleep mode */
 	sleep_mode();
-	sleep_disable();
+	/* disable the watchdog, we do not need it any more */
 	wdt_disable();
 }
 
 void SleepLong(uint16_t cycles)
 {
-	// sleep the number of given cycles
-	// long-time tests showed that one sleep takes approx. 8.408 seconds
-	// we do not know if the difference to exactly 8 seconds is caused
-	// by inexactness of watchdog oscillator and/or by time needed for waking
-	// up and going to sleep again		
+	/* sleep the number of given cycles
+	   long-time tests showed that one sleep takes approx. 8.408 seconds
+	   we do not know if the difference to exactly 8 seconds is caused
+	   by inexactness of watchdog oscillator and/or by time needed for waking
+	   up and going to sleep again */
 	for (uint16_t i=0 ; i<cycles; i++)
 	{
+		/* initialize the watchdog */
 		wdt_init(WDTO_8S);
+		/* and sleep */
 		sleep_now();
 	}
 }
 
+/* Get the number of sleep cycles from spark/photon */
+/* use PB4 (Attiny) connected to D1 of Spark as CLK (Attiny output) */
+/* use PB3 (Attiny) connected to D0 of Spark as Data (Attiny input) */
 uint16_t GetSleepTimeFromSpark()
 {
-	// use PB4 (Attiny) connected to D1 of Spark as CLK (Attiny output)
-	// use PB3 (Attiny) connected to D0 of Spark as Data (Attiny input)
-	
+	/* initialize to 0 */
 	uint16_t retval = 0b0000000000000000;
-	// read exactly 16 bit (2 bytes) from spark, MSB first
+	/* read exactly 16 bit (2 bytes) from spark/photon, MSB first */
 	for (int i=0;i<16;i++)
 	{
-		// shift left by one position
+		/* shift left by one position */
 		retval <<= 1;
-		// if PB3 = high then set last bit to 1 by increasing
+		/* read first bit: if PB3 = high then set last bit to 1 by increasing */
 		if (PINB & (1 << PINB3)) retval++;
-		// now toggle CLK signal with XOR
+		/* now toggle CLK signal with XOR */
 		PORTB ^= (1<<PINB4);
-		// we do not know delay, assuming 3ms is sufficient
-		// for spark to detect CLK toggle and set D1
-		// we tested 12 times with action plan -0001-0002-0003-0004 without any errors for 2ms delay.
-		// so we set to 3ms now and guess we are save.
+		/* we do not know delay, assuming 3ms is sufficient
+		   for spark to detect CLK toggle and set D1
+		   we tested 12 times with action plan -0001-0002-0003-0004 without any errors for 2ms delay.
+		   so we set to 3ms now and guess we are save. */
 		_delay_ms(3);
 	}
-
+	/* return the number of sleep cycles read from spark/photon */
 	return retval;
 }
 
-
+/* the main routine. startup, wait for spark/photon and display update, sleep */
 int main(void)
 {
-	// define the port usage: PINB2 is output for LDO shutdown
-	// define port PB4 as CLK output for serial communication with Spark
+	/* define the port usage: PINB2 is output for LDO shutdown
+	   define port PB4 as CLK output for serial communication with Spark */
 	DDRB=0b00010100;
-	//MCUCR=0b00010000;
 
 	//PCMSK = (1<<PCINT1);	// pin change mask: listen to portb bit 2
 	//GIMSK |= (1<<PCIE);	// enable PCINT interrupt
