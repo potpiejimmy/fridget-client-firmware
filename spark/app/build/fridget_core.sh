7ddbb350a5480742ec314b4b33da5445fda7472e
Thorsten <thorsten@potpiejimmy.de>
2015-11-21 21:26:47 +0100

    modified path documentation

--- spark/app/build/fridget_core.sh
+++ spark/app/build/fridget_core.sh
@@ -1,4 +1,4 @@
 # Copy this script to firmware/main
 # Note: link the fridget src folder to ../user/applications/fridget
-#ln -s /Users/thorsten/develop/fridget/fridget-client-firmware/spark/trunk/app/src ../user/applications/fridget
+#ln -s /Users/thorsten/develop/fridget/fridget-client-firmware/spark/app/src ../user/applications/fridget
 make PLATFORM=core APP=fridget TARGET_FILE=core-firmware all program-dfu
